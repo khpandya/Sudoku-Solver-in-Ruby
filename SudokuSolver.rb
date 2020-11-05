b385fa42d3479cdc4d54a943f1aacec4a2fa0c3c
krishna <khpandya@mun.ca>
2020-11-05 01:21:07 -0330

    fixed constant dynamic assignment error - code runs now

--- SudokuSolver.rb
+++ SudokuSolver.rb
@@ -85,41 +85,41 @@ def generateBoardCombination(rows)
 end
 
 def getCol(board)
-    Col1=[]
-    Col2=[]
-    Col3=[]
-    Col4=[]
-    Col5=[]
-    Col6=[]
-    Col7=[]
-    Col8=[]
-    Col9=[]
+    col1=[]
+    col2=[]
+    col3=[]
+    col4=[]
+    col5=[]
+    col6=[]
+    col7=[]
+    col8=[]
+    col9=[]
     for num in (0..8)
-        Col1<<board[num][0]
-        Col2<<board[num][1]
-        Col3<<board[num][2]
-        Col4<<board[num][3]
-        Col5<<board[num][4]
-        Col6<<board[num][5]
-        Col7<<board[num][6]
-        Col8<<board[num][7]
-        Col9<<board[num][8]
+        col1<<board[num][0]
+        col2<<board[num][1]
+        col3<<board[num][2]
+        col4<<board[num][3]
+        col5<<board[num][4]
+        col6<<board[num][5]
+        col7<<board[num][6]
+        col8<<board[num][7]
+        col9<<board[num][8]
     end
-    colValues=[Col1,Col2,Col3,Col4,Col5,Col6,Col7,Col8,Col9]
+    colValues=[col1,col2,col3,col4,col5,col6,col7,col8,col9]
     return colValues
 end
 
 def getBox(board)
-    Box1=[board[0][0],board[0][1],board[0][2],board[1][0],board[1][1],board[1][2],board[2][0],board[2][1],board[2][2]]
-    Box2=[board[0][3],board[0][4],board[0][5],board[1][3],board[1][4],board[1][5],board[2][3],board[2][4],board[2][5]]
-    Box3=[board[0][6],board[0][7],board[0][8],board[1][6],board[1][7],board[1][8],board[2][6],board[2][7],board[2][8]]
-    Box4=[board[3][0],board[3][1],board[3][2],board[4][0],board[4][1],board[4][2],board[5][0],board[5][1],board[5][2]]
-    Box5=[board[3][3],board[3][4],board[3][5],board[4][3],board[4][4],board[4][5],board[5][3],board[5][4],board[5][5]]
-    Box6=[board[3][6],board[3][7],board[3][8],board[4][6],board[4][7],board[4][8],board[5][6],board[5][7],board[5][8]]
-    Box7=[board[6][0],board[6][1],board[6][2],board[7][0],board[7][1],board[7][2],board[8][0],board[8][1],board[8][2]]
-    Box8=[board[6][3],board[6][4],board[6][5],board[7][3],board[7][4],board[7][5],board[8][3],board[8][4],board[8][5]]
-    Box9=[board[6][6],board[6][7],board[6][8],board[7][6],board[7][7],board[7][8],board[8][6],board[8][7],board[8][8]]
-    boxValues=[Box1,Box2,Box3,Box4,Box5,Box6,Box7,Box8,Box9]
+    box1=[board[0][0],board[0][1],board[0][2],board[1][0],board[1][1],board[1][2],board[2][0],board[2][1],board[2][2]]
+    box2=[board[0][3],board[0][4],board[0][5],board[1][3],board[1][4],board[1][5],board[2][3],board[2][4],board[2][5]]
+    box3=[board[0][6],board[0][7],board[0][8],board[1][6],board[1][7],board[1][8],board[2][6],board[2][7],board[2][8]]
+    box4=[board[3][0],board[3][1],board[3][2],board[4][0],board[4][1],board[4][2],board[5][0],board[5][1],board[5][2]]
+    box5=[board[3][3],board[3][4],board[3][5],board[4][3],board[4][4],board[4][5],board[5][3],board[5][4],board[5][5]]
+    box6=[board[3][6],board[3][7],board[3][8],board[4][6],board[4][7],board[4][8],board[5][6],board[5][7],board[5][8]]
+    box7=[board[6][0],board[6][1],board[6][2],board[7][0],board[7][1],board[7][2],board[8][0],board[8][1],board[8][2]]
+    box8=[board[6][3],board[6][4],board[6][5],board[7][3],board[7][4],board[7][5],board[8][3],board[8][4],board[8][5]]
+    box9=[board[6][6],board[6][7],board[6][8],board[7][6],board[7][7],board[7][8],board[8][6],board[8][7],board[8][8]]
+    boxValues=[box1,box2,box3,box4,box5,box6,box7,box8,box9]
     return boxValues
 
 
@@ -186,4 +186,5 @@ def bruteSolve()
     puts "No valid combination found"
 end
 
-solution=bruteSolve()                           
+solution=bruteSolve()
+end                          
