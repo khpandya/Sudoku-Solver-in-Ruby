62dc014da59f31a45704fc1492551ab9115f2ecc
krishna <khpandya@mun.ca>
2020-11-05 01:11:41 -0330

    added getBox function

--- SudokuSolver.rb
+++ SudokuSolver.rb
@@ -109,6 +109,20 @@ def getCol(board)
     return colValues
 end
 
+def getBox(board)
+    Box1=[board[0][0],board[0][1],board[0][2],board[1][0],board[1][1],board[1][2],board[2][0],board[2][1],board[2][2]]
+    Box2=[board[0][3],board[0][4],board[0][5],board[1][3],board[1][4],board[1][5],board[2][3],board[2][4],board[2][5]]
+    Box3=[board[0][6],board[0][7],board[0][8],board[1][6],board[1][7],board[1][8],board[2][6],board[2][7],board[2][8]]
+    Box4=[board[3][0],board[3][1],board[3][2],board[4][0],board[4][1],board[4][2],board[5][0],board[5][1],board[5][2]]
+    Box5=[board[3][3],board[3][4],board[3][5],board[4][3],board[4][4],board[4][5],board[5][3],board[5][4],board[5][5]]
+    Box6=[board[3][6],board[3][7],board[3][8],board[4][6],board[4][7],board[4][8],board[5][6],board[5][7],board[5][8]]
+    Box7=[board[6][0],board[6][1],board[6][2],board[7][0],board[7][1],board[7][2],board[8][0],board[8][1],board[8][2]]
+    Box8=[board[6][3],board[6][4],board[6][5],board[7][3],board[7][4],board[7][5],board[8][3],board[8][4],board[8][5]]
+    Box9=[board[6][6],board[6][7],board[6][8],board[7][6],board[7][7],board[7][8],board[8][6],board[8][7],board[8][8]]
+    boxValues=[Box1,Box2,Box3,Box4,Box5,Box6,Box7,Box8,Box9]
+    return boxValues
+
+
 def validBoard(combination)
     #rowArray is same as combination
     colArray=getCol(combination)
@@ -172,4 +186,4 @@ def bruteSolve()
     puts "No valid combination found"
 end
 
-                                  
+solution=bruteSolve()                           
